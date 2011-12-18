3b80e419008fd760337a62374efc5e4db86061e2
liangpig1@gmail.com <liangpig1@gmail.com@551fd8be-ad47-5d69-19b9-b37bbe80da92>
2011-12-18 13:58:28 +0000

    基本框架～

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+define('THINK_PATH', './ThinkPHP/');
+
+define('APP_NAME', 'WordDestructor');
+define ('APP_PATH', './WordDestructor/');
+
+require(THINK_PATH."/ThinkPHP.php");
+App::run();
+
+?>
