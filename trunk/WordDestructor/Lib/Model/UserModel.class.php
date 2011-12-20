ad7093d0834c1d53bb67e6014623c4427315838d
proevolytx@gmail.com <proevolytx@gmail.com@551fd8be-ad47-5d69-19b9-b37bbe80da92>
2011-12-20 13:22:10 +0000

    加了OOAD作业里的方法

--- trunk/WordDestructor/Lib/Model/UserModel.class.php
+++ trunk/WordDestructor/Lib/Model/UserModel.class.php
@@ -1,5 +1,45 @@
 <?php
 class UserModel extends Model
 {
+    public function getUserByID($id)
+    {
+        return D("User")->where("UserID=".$id)->find();
+    }
+    
+    public function getUsersByName($name)
+    {
+        return D("User")->where('UserName="'.$name.'"')->select();
+    }
+    
+    public function getUsers()
+    {
+        return D("User")->select();
+    }
+    
+    public function removeUser($id)
+    {
+        D("User")->where("UserID=".$id)->delete();
+    }
+    
+    public function insertUser($info)
+    {
+        D("User")->add($info);
+    }
+    
+    public function updateUserInfo($info)
+    {
+        D("User")->save($info);
+    }
+    
+    public function validateUser($name, $pass) 
+    {
+        $catalog = D("user");
+        $condition["UserName"] = '"'.$name.'"';
+        $condition["Password"] = '"'.$pass.'"';
+        $user = $catalog->where($condition)->find();
+        if ($user && $user["isFrozen"] == false)
+            return true;
+        return false;
+    }
 }
 ?>
