7a330a491e9ecc293f78fa879a0c838ac2dc0282
liangpig1@gmail.com <liangpig1@gmail.com@551fd8be-ad47-5d69-19b9-b37bbe80da92>
2011-12-21 10:35:17 +0000

    用户模块～～～

--- trunk/WordDestructor/Lib/Action/Admin/AdminAction.class.php
+++ trunk/WordDestructor/Lib/Action/Admin/AdminAction.class.php
@@ -5,23 +5,27 @@ class AdminAction extends Action
 
 	public function authorize($userID)
 	{
+		if (!$userID) $userID = $_SESSION["uid"];
 		$userDao = D("User");
+		$user = $userDao->getUserByID($userID);
 		if ($user = $userDao->getUserByID($userID))
 		{
 			if ($user["authority"]==true) return true; else return false;
 		}
-		else return false
+		else return false;
 	}
 
 	public function freezeUser($userID)
 	{
-		if (!$userID) $userID = $_GET["userID"]
+		if (!$userID) $userID = $_GET["userID"];
 		if (A("User")->islogin() && $this->authorize()) {
 			$userDao = D("User");
 			$user = $userDao->getUserByID($userID);
 			if ($user) {
-				$user['state'] = false;
-				$userDao->updateUser($data);
+				dump($user);
+				$user['state'] = 1;
+				$userDao->updateUser($user);
+				$this->redirect("Home-index/home", null, 1, "用户冻结成功");
 			}
 			else {
 				$this->errMsg = "找不到指定用户";
@@ -36,13 +40,15 @@ class AdminAction extends Action
 
 	public function unfreezeUser($userID)
 	{
-		if (!$userID) $userID = $_GET["userID"]
+		if (!$userID) $userID = $_GET["userID"];
 		if (A("User")->islogin() && $this->authorize()) {
 			$userDao = D("User");
 			$user = $userDao->getUserByID($userID);
 			if ($user) {
-				$user['state'] = true;
-				$userDao->updateUser($data);
+				dump($user);
+				$user['state'] = 0;
+				$userDao->updateUser($user);
+				$this->redirect("Home-index/home", null, 1, "用户解冻成功");
 			}
 			else {
 				$this->errMsg = "找不到指定用户";
@@ -55,12 +61,13 @@ class AdminAction extends Action
 		}
 	}
 
-	public function deleteUser($userID)
+	public function removeUser($userID)
 	{
-		if (!$userID) $userID = $_GET["userID"]
+		if (!$userID) $userID = $_GET["userID"];
 		if (A("User")->islogin() && $this->authorize()) {
 			$userDao = D("User");
 			$userDao->removeUserByID($userID); //TODO connected with relational model
+			$this->redirect("Home-index/home", null, 1, "删除成功");
 		}
 		else {
 			$this->errMsg = "无权限删除用户";
@@ -72,7 +79,9 @@ class AdminAction extends Action
 	{
 		if (A("User")->islogin() && $this->authorize()) {
 			$userDao = D("User");
-			return $userDao->getAllUsers();
+			$userList = $userDao->getAllUsers();
+			$this->assign("userList", $userList);
+			$this->display("Admin:Admin:list");
 		}
 		else {
 			$this->errMsg = "无权限查询用户";
