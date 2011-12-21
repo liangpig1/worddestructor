6abee03395819c9f955707144dee387226824c60
sefetion@gmail.com <sefetion@gmail.com@551fd8be-ad47-5d69-19b9-b37bbe80da92>
2011-12-21 03:35:28 +0000

    无视我。。

--- trunk/WordDestructor/Lib/Action/Home/WordlibraryAction.class.php
+++ trunk/WordDestructor/Lib/Action/Home/WordlibraryAction.class.php
@@ -3,7 +3,25 @@ class WordlibraryAction extends Action
 {
 	public function deleteLibrary($libraryID)
 	{
+		$libraryDao = D("library");
 	}
+	
+	public function mergeLibrary($libraryID1,$libraryID2)
+	{
+	}
+	
+	public function newLibrary($listofword,$discription)
+	{
+	}
+	
+	public function listLibrariesByCondition($conditon = null)
+	{
+	}
+	
+	public function listLibraryByUser($userID)
+	{
+	}
+	
 }
 
 ?>
