945b15b0ebf824c5af489504e9737cb2515fb945
proevolytx@gmail.com <proevolytx@gmail.com@551fd8be-ad47-5d69-19b9-b37bbe80da92>
2011-12-21 10:17:49 +0000

    似乎wordlist没什么方法啊？wordRef太强大了啊有木有！亲！！

--- WordDestructor/Lib/Model/WordlistModel.class.php
+++ WordDestructor/Lib/Model/WordlistModel.class.php
@@ -1,7 +1,31 @@
 <?php
 class WordlistModel extends Model
 {
-
+    public function getListsByUser($userId)
+    {
+        return $this->where("userId=".$userId)->select();
+    }
+    
+    public function getAllLists()
+    {
+        return $this->select();
+    }
+    
+    public function deleteWordList($listId)
+    {
+        $this->where("id=".$listId)->delete();
+    }
+    
+    //新建一个空词单
+    public function addWordList($listInfo)
+    {
+        $this->add($listInfo);
+    }
+    
+    public function updateWordList($listInfo)
+    {
+        $this->save($listInfo);
+    }
 }
 
 ?>
